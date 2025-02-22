d1c216a47e22d3c67cc7950324b3412cd591fdb7
RhexNiebres <rhexniebres@gmail.com>
2025-02-21 18:25:12 -0800

    set up ejs with paths of index and new

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 const express = require("express");
 const app = express();
+const path = require("node:path");
 
-app.get("/", (req, res) => res.send("Hello, world!"));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-const PORT = 3000;
+const links = [
+  { href: "/", text: "messages" },
+  { href: "new", text: "post a new message" },
+];
+
+app.get("/", (req, res) => {
+    res.render("index", { links: links});
+  });
+  app.get("new", (req, res) => {
+    res.render("new", { links: links });
+  });
+  
+const port = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}!`);
+});
+
+app.use((req, res, next) => {
+  throw new Error("OH NO! this page does not exist");
+  // or next(new Error("OH NO!"));
 });
