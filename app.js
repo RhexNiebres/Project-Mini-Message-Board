b6b32b2050d73540605bb3e6a757b3f4e4e49433
RhexNiebres <rhexniebres@gmail.com>
2025-02-22 19:51:52 -0800

    moved to proper folder

--- app.js
+++ app.js
@@ -57,5 +57,5 @@ app.listen(port, () => {
 });
 
 app.use((req, res) => {
-  res.status(404).render("/errors/404", { message: "Page not found!" });
+  res.status(404).render("404", { message: "Page not found!" });
 });
